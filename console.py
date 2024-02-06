f842182c875a61ce89dacad6b38b433bfdc62e61
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-02-06 16:42:35 +0000

    Updated the command interpreter #2
    This update handles unknown commands and also introduces a mini case
    insensitivity for the 'EOF' command. I did this to ensure all method
    names are lower case. This was achieved by overriding the `precmd()`
    method. Furthermore, there's a specific method for the 'EOF' now.

--- console.py
+++ console.py
@@ -2,7 +2,6 @@
 
 """Implements the command interpreter."""
 
-
 import cmd
 
 
@@ -13,14 +12,25 @@ class HBNBCommand(cmd.Cmd):
 
     @staticmethod
     def do_quit(_) -> bool:
-        """Quit command to exit the program"""
+        """Quit command to exit the program."""
         return True
 
-    # `EOF` and `quit` do about the same thing so... B-)
-    do_EOF = do_quit
+    @staticmethod
+    def do_eof(_) -> bool:
+        """Handles the Ctrl+D signal (EOF)."""
+        print()
+        return True
 
     def emptyline(self) -> None:
         pass
+
+    def default(self, line: str) -> None:
+        print(f"Unknown command: {line}")
+
+    def precmd(self, line):
+        if line and line == "EOF":
+            return line.lower()
+        return line
 
 
 if __name__ == "__main__":
